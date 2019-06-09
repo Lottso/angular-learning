4483ed230be9f2550d7da8685468af9a8c5b8a5f
Vincenty <vincent@san-i.co.il>
2019-06-09 20:08:41 +0300

    Before databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from  '@angular/common/http';
+import { ServerComponent } from './server/server.component'
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { JourneyComponent } from './journey/journey.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    JourneyComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
